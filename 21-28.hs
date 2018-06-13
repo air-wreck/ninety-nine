5b0fe7698f970c455d3897ca35900378a35a6f9a
Eric <zheng.eric.y@gmail.com>
2018-06-13 11:46:30 -0400

    finish problems 21-28

--- 21-28.hs
+++ 21-28.hs
@@ -31,13 +31,30 @@ rnd_permu xs = rnd_select xs (length xs)
 -- problem 26
 combinations :: Int -> [a] -> [[a]]
 combinations 0 _  = [[]]
-combinations n xs = foldl (\acc x -> acc ++ x) [] (map (\(i, el) ->
+combinations n xs = foldl (++) [] (map (\(i, el) ->
   map (\subel -> el : subel) $ combinations (n-1) (drop (i+1) xs))
     $ zip [0..] xs)
 
 -- problem 27
--- i'll do this later because words like "multinomial coefficients" and
--- "disjoint subset" scare me
+-- we can get rid of the (Eq a) constraint by zipping with [0..], but whatever
+-- part (a)
+group3 :: Eq a => [a] -> [[[a]]]
+group3 xs = foldl (++) [] $ map (\g2 ->
+  map (\g3 -> [g2, g3, filter (\x -> (x `notElem` g2) && (x `notElem` g3)) xs])
+    (combinations 3 (filter (\x -> x `notElem` g2) xs))) $ combinations 2 xs
+-- part (b)
+-- we use the identifier group' to avoid conflicting with group from Data.List
+group' :: Eq a => [Int] -> [a] -> [[[a]]]
+group' ns xs = foldl (\acc n ->
+  if length acc == 0
+    then map (\x -> [x]) (combinations n xs)
+  else foldl (++) [] $ map (\group ->
+    map (\combo -> group ++ [combo])
+        (combinations n (filter (\x -> isnt x group) xs))) acc) [] ns
+-- auxiliary funtion tests if x isn't in a nested list
+isnt :: (Foldable t, Eq a) => a -> t (t a) -> Bool
+isnt x = foldl (\acc -> (&& acc) . (notElem x)) True
+
 
 -- problem 28
 -- part (a)
